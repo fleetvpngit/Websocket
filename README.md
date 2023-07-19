75fd5c8879c9539bbddcf28db95797d32978b10a
fleetvpngit <107276018+fleetvpngit@users.noreply.github.com>
2023-07-18 23:40:54 -0300

    upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# INSTALAR WEBSOCKET SECURITY
+
+apt update -y && apt upgrade -y && wget https://raw.githubusercontent.com/fleetvpngit/Websocket/main/websocket.sh && chmod 777 websocket.sh && ./websocket.sh
--- websocket.sh
+++ websocket.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-sudo apt update
-sudo apt upgrade -y
 cd /etc/SSHPlus/ && wget https://github.com/fleetvpngit/Websocket/raw/main/files/WebSocket && wget https://raw.githubusercontent.com/fleetvpngit/Websocket/main/files/pub.key && wget https://raw.githubusercontent.com/fleetvpngit/Websocket/main/files/priv.pem && chmod 777 WebSocket && cd $HOME
 mkdir /opt/sshplus
 echo > /opt/sshplus/sshplus
