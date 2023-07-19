97e3cc7d6971e8711c7ee75481cd7ef56beddea4
fleetvpngit <107276018+fleetvpngit@users.noreply.github.com>
2023-07-19 11:01:21 -0300

    Update installwebsocket.sh

--- installwebsocket.sh
+++ installwebsocket.sh
@@ -14,29 +14,16 @@ exibir_status() {
 
 exibir_status "INSTALADOR DO WEBSOCKET"
 
-# Verificar se o arquivo WebSocket.sh já existe na pasta raiz
-if [ -f "/etc/SSHPlus/WebSocket" ]; then
-    exibir_status "${cor_vermelho}O arquivo /etc/SSHPlus/WebSocket já existe. Será excluído antes de continuar.${cor_padrao}"
-    rm -rf /etc/SSHPlus/WebSocket
-fi
+exibir_status "${cor_verde}Baixando e instalando arquivos do WebSocket...${cor_padrao}"
 
-# Verificar se o arquivo pub.key já existe na pasta /etc/SSHPlus
-if [ -f "/etc/SSHPlus/pub.key" ]; then
-    exibir_status "${cor_vermelho}O arquivo /etc/SSHPlus/pub.key já existe. Será excluído antes de continuar.${cor_padrao}"
-    rm -f /etc/SSHPlus/pub.key
-fi
+# Baixar os arquivos necessários e dar permissões
+cd /etc/SSHPlus/ && wget https://github.com/fleetvpngit/Websocket/raw/main/files/WebSocket && wget https://raw.githubusercontent.com/fleetvpngit/Websocket/main/files/pub.key && wget https://raw.githubusercontent.com/fleetvpngit/Websocket/main/files/priv.pem && chmod 777 WebSocket && cd $HOME
 
-# Verificar se o arquivo priv.pem já existe na pasta /etc/SSHPlus
-if [ -f "/etc/SSHPlus/priv.pem" ]; then
-    exibir_status "${cor_vermelho}O arquivo /etc/SSHPlus/priv.pem já existe. Será excluído antes de continuar.${cor_padrao}"
-    rm -f /etc/SSHPlus/priv.pem
-fi
+exibir_status "${cor_verde}Configurando o diretório SSHPlus...${cor_padrao}"
 
-# Verificar se o diretório /opt/sshplus já existe
-if [ -d "/opt/sshplus" ]; then
-    exibir_status "${cor_vermelho}O diretório /opt/sshplus já existe. Será excluído antes de continuar.${cor_padrao}"
-    rm -rf /opt/sshplus
-fi
+# Criar diretório /opt/sshplus e criar arquivo vazio
+mkdir /opt/sshplus
+echo > /opt/sshplus/sshplus
 
 exibir_status "${cor_verde}Configurando o WebSocket...${cor_padrao}"
 
