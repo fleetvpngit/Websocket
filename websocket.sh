8c0b030cc6704de9ee8b1c041e6b1f45aad923ef
fleetvpngit <107276018+fleetvpngit@users.noreply.github.com>
2023-07-18 23:46:38 -0300

    Update websocket.sh

--- websocket.sh
+++ websocket.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
+
+# Atualizar e fazer upgrade do sistema
 cd /etc/SSHPlus/ && wget https://github.com/fleetvpngit/Websocket/raw/main/files/WebSocket && wget https://raw.githubusercontent.com/fleetvpngit/Websocket/main/files/pub.key && wget https://raw.githubusercontent.com/fleetvpngit/Websocket/main/files/priv.pem && chmod 777 WebSocket && cd $HOME
 mkdir /opt/sshplus
 echo > /opt/sshplus/sshplus
-screen -dmS novoWS /etc/SSHPlus/WebSocket -proxy_port 0.0.0.0:80 -msg=SUA_MENSAGEM_AQUI
+read -p "Digite a porta desejada (ex: 80): " porta
+screen -dmS novoWS /etc/SSHPlus/WebSocket -proxy_port 0.0.0.0:$porta -msg=SUA_MENSAGEM_AQUI
+proxy_status=$(screen -ls | grep novoWS)
+
+if [ -n "$proxy_status" ]; then
+    echo "O proxy está em execução na porta $porta."
+else
+    echo "Houve um erro ao iniciar o proxy."
+fi
