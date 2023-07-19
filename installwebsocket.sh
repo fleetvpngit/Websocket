143342d553d15b4e78acbaf262a7d704032d90bc
fleetvpngit <107276018+fleetvpngit@users.noreply.github.com>
2023-07-19 01:06:51 -0300

    Update installwebsocket.sh

--- installwebsocket.sh
+++ installwebsocket.sh
@@ -38,15 +38,26 @@ if [ -d "/opt/sshplus" ]; then
     rm -rf /opt/sshplus
 fi
 
-exibir_status "${cor_verde}Baixando e instalando arquivos do WebSocket...${cor_padrao}"
+exibir_status "${cor_verde}Configurando o WebSocket...${cor_padrao}"
 
-# Baixar os arquivos necessários e dar permissões
-cd /etc/SSHPlus/ && wget https://github.com/fleetvpngit/Websocket/raw/main/files/WebSocket && wget https://raw.githubusercontent.com/fleetvpngit/Websocket/main/files/pub.key && wget https://raw.githubusercontent.com/fleetvpngit/Websocket/main/files/priv.pem && chmod 777 WebSocket && cd $HOME
+# Solicitar a porta desejada para o WebSocket
+read -p $'\e[1mDigite a porta desejada para o WebSocket (ex: 80, 8080): \e[0m' porta
 
-exibir_status "${cor_verde}Configurando o diretório SSHPlus...${cor_padrao}"
+# Solicitar a mensagem desejada para o WebSocket
+read -p $'\e[1mDigite a mensagem desejada para o WebSocket: \e[0m' mensagem
 
-# Criar diretório /opt/sshplus e criar arquivo vazio
-mkdir /opt/sshplus
-echo > /opt/sshplus/sshplus
+# Executar comando com a porta e a mensagem fornecidas
+screen -dmS novoWS /etc/SSHPlus/WebSocket -proxy_port 0.0.0.0:$porta -msg="$mensagem"
 
-screen -dmS novoWS /etc/SSHPlus/WebSocket -proxy_port 0.0.0.0:6666 -msg=SUA_MENSAGEM_AQUI
+sleep 1
+
+exibir_status "${cor_verde}Verificando o status do proxy...${cor_padrao}"
+
+# Verificar se o proxy está em execução
+proxy_status=$(screen -list | grep "novoWS")
+
+if [ -n "$proxy_status" ]; then
+    exibir_status "${cor_verde}O proxy está em execução na porta $porta.${cor_padrao}"
+else
+    exibir_status "${cor_vermelho}Houve um erro ao iniciar o proxy.${cor_padrao}"
+fi
